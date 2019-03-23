60b97c55f08f8f005a44387d4b3da7f24690bf52
Tim van Dijen <tvdijen@gmail.com>
2019-03-23 11:00:26 +0100

    Convert assertions to Webmozart

--- lib/Auth/Source/Radius.php
+++ lib/Auth/Source/Radius.php
@@ -2,6 +2,8 @@
 
 namespace SimpleSAML\Module\radius\Auth\Source;
 
+use Webmozart\Assert\Assert;
+
 /**
  * RADIUS authentication source.
  *
@@ -76,8 +78,8 @@ class Radius extends \SimpleSAML\Module\core\Auth\UserPassBase
      */
     public function __construct($info, $config)
     {
-        assert(is_array($info));
-        assert(is_array($config));
+        Assert::isArray($info);
+        Assert::isArray($config);
 
         // Call the parent constructor first, as required by the interface
         parent::__construct($info, $config);
@@ -125,8 +127,8 @@ class Radius extends \SimpleSAML\Module\core\Auth\UserPassBase
      */
     protected function login($username, $password)
     {
-        assert(is_string($username));
-        assert(is_string($password));
+        Assert::string($username);
+        Assert::string($password);
 
         $radius = radius_auth_open();
 
